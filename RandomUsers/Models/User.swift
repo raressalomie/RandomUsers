a956ca91b11a11059d0f210f5e188b1f8f61d117
Tiberiu Rares Salomie <raressalomie@gmail.com>
2025-07-24 14:34:23 +0200

    Created model for the Result

--- RandomUsers/Models/User.swift
+++ RandomUsers/Models/User.swift
@@ -5,4 +5,57 @@
 //  Created by Tiberiu Rares Salomie on 24/7/25.
 //
 
+import SwiftData
 import Foundation
+
+struct Result: Codable {
+    let results: [User]
+    let inf: Info
+}
+
+struct Info: Codable {
+    let seed: String
+    let results: Int
+    let page: Int
+    let version: String
+}
+
+struct User: Codable {
+    let gender: String
+    let name: Name
+    let location: Location
+    let email: String
+    let registered: Registered
+    let picture: Picture
+    
+    struct Location: Codable {
+        let street: Street
+        let city: String
+        let state: String
+        let country: String
+        let postcode: String
+        
+        struct Street: Codable {
+            let number: Int
+            let name: String
+        }
+    }
+    
+    struct Registered: Codable {
+        let date: String
+        let age: Int
+    }
+    
+    struct Name: Codable {
+        let title: String
+        let first: String
+        let last: String
+    }
+    
+    struct Picture: Codable {
+        let large: String
+        let medium: String
+        let thumbnail: String
+    }
+}
+
