b84cde16800bd949c6f61434d0dacd6a88991634
Tiberiu Rares Salomie <raressalomie@gmail.com>
2025-07-24 18:36:09 +0200

    Modified the contentView to better fit the purpose of my app

--- RandomUsers/ContentView.swift
+++ RandomUsers/ContentView.swift
@@ -10,36 +10,35 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State private var users = [User]()
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        List(users, id: \.name.first) { user in
+            Text(user.name.first)
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "http://api.randomuser.me/?results=40") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResult = try? JSONDecoder().decode(Result.self, from: data) {
+                users = decodedResult.results
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        } catch {
+            print("Error: \(error.localizedDescription)")
         }
     }
 
-    private func addItem() {
+   /* private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
             modelContext.insert(newItem)
@@ -49,10 +48,10 @@ struct ContentView: View {
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
-                modelContext.delete(items[index])
+                modelContext.delete(users[index])
             }
         }
-    }
+    }*/
 }
 
 #Preview {
