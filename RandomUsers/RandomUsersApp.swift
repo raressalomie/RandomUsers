d61e9fc212d05d6a2b152096be798396e57c9eab
Tiberiu Rares Salomie <trsalomie@minsait.com>
2025-07-24 12:36:30 +0200

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,32 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Tiberiu Rares Salomie on 24/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RandomUsersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
